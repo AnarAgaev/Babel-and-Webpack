4f22d613c09a739bede7c1f1ccd984bb691a5721
Anar Agaev <anar.n.agaev@gmail.com>
2020-02-22 17:23:19 +0300

    shared the logic of launch webpack to  development or  production

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss'
 
-const App = () => <p>Hello</p>;
+const App = () => <p>This is WebPack React App</p>;
 ReactDOM.render(<App />, document.getElementById('root'));
